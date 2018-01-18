c58b8fd6be6af60db35a80ebb85f1fb1ad6dc396
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-01-18 20:28:52 +0800

    disini saya membuat isi dari variabel yang di deklarasikan pada variabel di atasnya yaitu private yang didalamnya terdapat nilai value

--- Tugas1.cpp
+++ Tugas1.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class Jenispiranha
+class Spesiespiranha
 
 {
 private:
@@ -13,3 +13,19 @@ private:
     string warnasisik;
     int panjanggigi;
     int habitatpiranha;
+    
+public:
+
+
+    Jenispiranha (string namapiranha, string jenispiranha, string namalatin, string warnasisik, 
+	int panjanggigi, int habitatpiranha )
+
+    {
+    	Spesiespiranha::namapiranha = namapiranha;
+        Spesiespiranha::jenispiranha = jenispiranha;
+        Spesiespiranha::namalatin = namalatin;
+        Spesiespiranha::warnasisik = warnasisik;
+        Spesiespiranha::panjanggigi = panjanggigi;
+        Spesiespiranha::habitatpiranha = habitatpiranha;
+        
+    }
