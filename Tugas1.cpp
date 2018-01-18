fa6fd36a1e6696e0492367771fe3bec8d78acefd
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-01-18 21:23:38 +0800

    ini adalah output an dari beberapa deklarasi di atas yang terhimpun dalam class

--- Tugas1.cpp
+++ Tugas1.cpp
@@ -2,48 +2,71 @@
 #include <string>
 using namespace std;
 
-class Spesiespiranha
+class Speciespiranha
 
 {
 private:
 
 	string namapiranha;
-    string jenispiranha;
-    string namalatin;
     string warnasisik;
+    string namalatin;
+    string habitatpiranha;
     int panjanggigi;
-    int habitatpiranha;
+    int panjangtubuh;
     
+
 public:
 
 
-    Jenispiranha (string namapiranha, string jenispiranha, string namalatin, string warnasisik, 
-	int panjanggigi, int habitatpiranha )
+    Speciespiranha (string namapiranha, string warnasisik, string namalatin, string habitatpiranha, 
+	int panjanggigi, int panjangtubuh )
 
     {
-    	Spesiespiranha::namapiranha = namapiranha;
-        Spesiespiranha::jenispiranha = jenispiranha;
-        Spesiespiranha::namalatin = namalatin;
-        Spesiespiranha::warnasisik = warnasisik;
-        Spesiespiranha::panjanggigi = panjanggigi;
-        Spesiespiranha::habitatpiranha = habitatpiranha;
+    	Speciespiranha::namapiranha = namapiranha;
+        Speciespiranha::warnasisik = warnasisik;
+        Speciespiranha::namalatin = namalatin;
+        Speciespiranha::habitatpiranha = habitatpiranha;
+        Speciespiranha::panjanggigi = panjanggigi;
+        Speciespiranha::panjangtubuh = panjangtubuh;
         
     }
-    
+
+
     void perolehInfo (void)
 
     {
-    	cout << "---------------------------------------------------------------- " << endl;
-    	cout << "-----------------------SP-------------------------------------- " << endl;
+    	cout << "---------------------------------------------------------------------------------" << endl;
+    	cout << "--------------------------------SPECIES PIRANHA----------------------------------" << endl;
 
     	
-        cout << "Nama Piranha         : " << namapiranha << endl;
-        cout << "Jenis Piranha             : " << jenispiranha << endl;
-        cout << "Nama Latin            : " << namalatin << endl;
-        cout << "Warna Sisik      	      : " << warnasisik << endl;
-        cout << "Panjang Gigi     : " << panjanggigi <<"Cm"<< endl;
-        cout << "Habitat Piranha      : " << habitatpiranha << endl;
+        cout << "Nama Piranha          	: " << namapiranha << endl;
+        cout << "Warna Sisik             : " << warnasisik << endl;
+        cout << "Nama Latin            	: " << namalatin << endl;
+        cout << "Habitat Piranha      	: " << habitatpiranha << endl;
+        cout << "Panjang Gigi (CM)    	: " << panjanggigi << endl;
+        cout << "Panjang Tubuh (CM)      : " << panjangtubuh << endl;
         
         cout << endl;
     };
     };
+
+int main()
+
+    { 
+	Speciespiranha PiranhaA("Boy Piranha", "Rainbow", 
+	"Pygopristis Denticulata","Blue Sea", 5, 20);
+	
+	 Speciespiranha PiranhaB("Syntax Error", "Kuning", 
+	"Pygocentrus nattereri","Laut Merah", 4, 30);
+	
+	 Speciespiranha PiranhaC("Kardon", "Merah", 
+	"Pygocentrus cariba","Laut palung maria", 10, 35);
+
+    PiranhaA.perolehInfo();
+    
+    PiranhaB.perolehInfo();
+    
+    PiranhaC.perolehInfo();
+    
+    return 0;
+}
