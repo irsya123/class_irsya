65b946ec2ac1799e36a35fb46b1795664d8a363b
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-01-18 20:19:57 +0800

    disini saya membuat variabel private yg berada didalam class

--- Tugas1.cpp
+++ Tugas1.cpp
@@ -3,3 +3,13 @@
 using namespace std;
 
 class Jenispiranha
+
+{
+private:
+
+	string namapiranha;
+    string jenispiranha;
+    string namalatin;
+    string warnasisik;
+    int panjanggigi;
+    int habitatpiranha;
